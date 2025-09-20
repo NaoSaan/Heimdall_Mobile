307fef9b934a08734bd9643d0c14d82f9b9db1be
MyTsKi <angeldavidcamacho2003@gmail.com>
2025-09-19 18:58:13 -0600

    Carpeta Views y logica de rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'views/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,71 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginScreen(),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  bool _obscurePassword = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('lib/assets/images/logo.jpeg', width: 80, height: 80),
-            const SizedBox(height: 20),
-            const Text('Heimdall'), // Texto
-
-            const SizedBox(height: 20),
-            // Campo de usuario
-            TextField(
-              decoration: const InputDecoration(labelText: 'Usuario'),
-            ),
-
-            const SizedBox(height: 20),
-            // Campo de contraseña
-            TextField(
-              obscureText: _obscurePassword,
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscurePassword
-                        ? Icons.visibility_off
-                        : Icons.visibility,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscurePassword = !_obscurePassword;
-                    });
-                  },
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 20),
-            // Botón de login
-            ElevatedButton(
-              onPressed: () {
-                // Aquí iría la lógica para el login
-              },
-              child: const Text('Iniciar sesión'),
-            ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      title: 'Heimdall',
+      initialRoute: LoginScreen.routeName,
+      debugShowCheckedModeBanner: false,
+      routes: {LoginScreen.routeName: (context) => const LoginScreen()},
     );
   }
 }
---
+++ lib/views/login.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String routeName = '/login';
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('lib/assets/images/logo.jpeg', width: 80, height: 80),
+            const SizedBox(height: 20),
+            const Text('Heimdall'), // Texto
+
+            const SizedBox(height: 20),
+            // Campo de usuario
+            TextField(decoration: const InputDecoration(labelText: 'Usuario')),
+
+            const SizedBox(height: 20),
+            // Campo de contraseña
+            TextField(
+              obscureText: _obscurePassword,
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+            // Botón de login
+            ElevatedButton(
+              onPressed: () {
+                // Aquí iría la lógica para el login
+              },
+              child: const Text('Iniciar sesión'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
